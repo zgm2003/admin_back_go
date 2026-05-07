805ca6ff685a3ca138eccc40a345667a33c566c9
zgm2003 <102711362+zgm2003@users.noreply.github.com>
2026-05-07 17:23:41 +0800

    fix: make chat removal migration runnable

--- database/migrations/20260507_remove_admin_chat.sql
+++ database/migrations/20260507_remove_admin_chat.sql
@@ -9,6 +9,11 @@ CREATE TEMPORARY TABLE `tmp_admin_chat_permissions` (
     `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY
 ) ENGINE=MEMORY;
 
+DROP TEMPORARY TABLE IF EXISTS `tmp_admin_chat_parent_permissions`;
+CREATE TEMPORARY TABLE `tmp_admin_chat_parent_permissions` (
+    `id` BIGINT UNSIGNED NOT NULL PRIMARY KEY
+) ENGINE=MEMORY;
+
 INSERT IGNORE INTO `tmp_admin_chat_permissions` (`id`)
 SELECT `id`
 FROM `permissions`
@@ -23,10 +28,14 @@ WHERE `platform` = 'admin'
       OR `code` LIKE 'chat\_%'
   );
 
+INSERT IGNORE INTO `tmp_admin_chat_parent_permissions` (`id`)
+SELECT `id`
+FROM `tmp_admin_chat_permissions`;
+
 INSERT IGNORE INTO `tmp_admin_chat_permissions` (`id`)
 SELECT child.`id`
 FROM `permissions` AS child
-JOIN `tmp_admin_chat_permissions` AS parent ON parent.`id` = child.`parent_id`
+JOIN `tmp_admin_chat_parent_permissions` AS parent ON parent.`id` = child.`parent_id`
 WHERE child.`platform` = 'admin';
 
 DELETE rp
@@ -37,6 +46,7 @@ DELETE p
 FROM `permissions` AS p
 JOIN `tmp_admin_chat_permissions` AS doomed ON doomed.`id` = p.`id`;
 
+DROP TEMPORARY TABLE IF EXISTS `tmp_admin_chat_parent_permissions`;
 DROP TEMPORARY TABLE IF EXISTS `tmp_admin_chat_permissions`;
 
 DROP TABLE IF EXISTS `chat_messages`;
